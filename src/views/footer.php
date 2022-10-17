6d67504f39977bbebcb2e709300d6b6815e5e2ad
BrigitaGrybaiteBartke <brigita.grybaite@gmail.com>
2022-10-17 18:11:02 +0300

    add footer

---
+++ src/views/footer.php
@@ -0,0 +1,44 @@
+<!-- <?php
+require_once "bootstrap.php";
+
+?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <footer class="text-center text-white fixed-bottom mt-5" style="background-color: #f1f1f1;">
+        <div class=" pt-2">
+            <section class="mb-2">
+                <!-- Linkedin -->
+                <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="https://www.linkedin.com/in/brigita-grybait%C4%97-bartk%C4%97-487403112" role="button" data-mdb-ripple-color="dark">
+                    <i class="bi bi-linkedin"></i>
+                </a>
+                <!-- Github -->
+                <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="https://github.com/BrigitaGrybaiteBartke" role="button" data-mdb-ripple-color="dark">
+                    <i class="bi bi-github"></i>
+                </a>
+                <!-- Gmail -->
+                <a class="btn btn-link btn-floating btn-lg text-dark m-1" role="button" data-mdb-ripple-color="dark" href="mailto:brigita.grybaite@gmail.com">
+                    <i class="bi bi-envelope"></i>
+                </a>
+            </section>
+            <!-- Copyright -->
+            <div class="text-center text-dark p-2" style="background-color: rgba(0, 0, 0, 0.2);">
+                <div class="aling-items-center">
+                    <span class="mx-5">All rights reserved &#169; <?php echo date("Y") ?></span>
+                </div>
+            </div>
+    </footer>
+
+</body>
+
+</html>
