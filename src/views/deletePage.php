7f0df3418701fac96facd4789dba9d46fa48aeb2
BrigitaGrybaiteBartke <brigita.grybaite@gmail.com>
2022-10-17 18:01:10 +0300

    add delete Page logic

---
+++ src/views/deletePage.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once "bootstrap.php";
+session_start();
+
+$base_url = 'http://localhost:8080/cms';
+$page = $entityManager->getRepository('models\Page')->findAll();
+
+// Delete page
+if (isset($_GET['delete'])) {
+    $page = $entityManager->find('models\Page', $_GET['delete']);
+    $entityManager->remove($page);
+    $entityManager->flush();
+    // redirect_to_root();
+    header('Location:' . $base_url . '/admin');
+}
